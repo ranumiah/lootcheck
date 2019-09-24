a90cd99ec47e1b602d1c036189711b0f9d6c02d2
Ranu <O@O.net>
2019-09-24 08:23:45 +0100

    add deposit feature to balance

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -8,4 +8,12 @@ describe('balanceReducer', () => {
         expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance }))
             .toEqual(balance);
     });
-})
+
+    it('deposits into the balance', () => {
+        const deposit = 10;
+        const initialState = 5;
+
+        expect(balanceReducer(initialState, { type: constants.DEPOSIT, deposit }))
+            .toEqual(initialState + deposit);
+    });
+});
