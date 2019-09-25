cab58aef6d18f6feaa7c2e7bbf7829e34be83369
Ranu <O@O.net>
2019-09-25 16:58:04 +0100

    store data with Cookies

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -1,12 +1,21 @@
 import balanceReducer from './balance';
+import balanceReducer2 from './balance';
 import * as constants from '../actions/constants';
 
 describe('balanceReducer', () => {
-    it('sets a balance', () => {
+    describe('when initialising', () => {
         const balance = 10;
 
-        expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance }))
-            .toEqual(balance);
+        it('sets a balance', () => {
+            expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance }))
+                .toEqual(balance);
+        });
+
+        describe('then re-initialising', () => {
+            it('reads the balance from cookies', () => {
+                expect(balanceReducer2(undefined, {})).toEqual(balance);
+            });
+        });
     });
 
     it('deposits into the balance', () => {
