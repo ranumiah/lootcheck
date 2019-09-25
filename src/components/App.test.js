d27d510df22e019879ba00a26a4026263f6e6483
Ranu <O@O.net>
2019-09-25 17:25:16 +0100

    connect redux to app and render the Wallet component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -8,4 +8,8 @@ describe('App', () => {
     it('renders correctly', () => {
         expect(app).toMatchSnapshot();
     });
+
+    it('contains a connected Wallet componet', () => {
+        expect(app.find('Connect(Wallet)').exists()).toBe(true)
+    });
 });
